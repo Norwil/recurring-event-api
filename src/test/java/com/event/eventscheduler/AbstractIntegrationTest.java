dc9e66e9be5d6d99d2b20585803f620685238a0b
Norwil <emretokluk@gmail.com>
2025-11-01 17:29:33 +0100

    Feat: Add Testcontainers for repository integration tests

---
+++ src/test/java/com/event/eventscheduler/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.event.eventscheduler;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
+@Testcontainers
+public class AbstractIntegrationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    private static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+}
